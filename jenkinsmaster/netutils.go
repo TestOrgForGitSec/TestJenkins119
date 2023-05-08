e6eaa2d2f364a6cf948d516400402ff26284a5fe
ambyy <anair@cloudbees.com>
2023-05-08 18:46:40 +0530

    CBCH-6968: Code to debug issues in HSBC env

---
+++ jenkinsmaster/netutils.go
@@ -0,0 +1,30 @@
+package jenkinsmaster
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+)
+
+type loggingTransport struct{}
+
+func (s *loggingTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	bytes, _ := httputil.DumpRequestOut(r, true)
+
+	resp, err := http.DefaultTransport.RoundTrip(r)
+	// err is returned after dumping the response
+
+	respBytes, _ := httputil.DumpResponse(resp, true)
+	bytes = append(bytes, respBytes...)
+
+	fmt.Printf("%s\n", bytes)
+
+	return resp, err
+}
+
+func GetHttpClient() http.Client {
+	client := http.Client{
+		Transport: &loggingTransport{},
+	}
+	return client
+}
