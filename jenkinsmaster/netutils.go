aee9f257a646c13b265ac0b2272956968928ec1d
ambyy <anair@cloudbees.com>
2023-05-29 23:25:09 +0530

    (fix)CBCE-3696: Error handling added before logging response payload (#10)

--- jenkinsmaster/netutils.go
+++ jenkinsmaster/netutils.go
@@ -19,9 +19,14 @@ func (s *loggingTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 	log.Debug().Msgf("Jenkins request URL: %s", r.URL.String())
 	resp, err := http.DefaultTransport.RoundTrip(r)
 
-	respBytes, _ := httputil.DumpResponse(resp, true)
-	bytes = append(bytes, respBytes...)
-	log.Debug().Msgf("Jenkins response is: %s", bytes)
+	if err != nil {
+		if resp != nil {
+			respBytes, _ := httputil.DumpResponse(resp, true)
+			bytes = append(bytes, respBytes...)
+			log.Debug().Msgf("Jenkins response is: %s", bytes)
+		}
+	}
+
 	return resp, err
 }
 
